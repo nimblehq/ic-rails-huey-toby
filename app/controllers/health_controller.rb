383ac527dde6fd907982acc625009aa291effd89
Tobias (Toby) Heuts <tobias@nimblehq.co>
2023-05-29 19:32:04 +0700

    [#24] Fix linter

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -11,20 +11,20 @@ class HealthController < ApplicationController
 
   def render_up
     respond_to do |format|
-      format.html { render html: html_status(color: 'green'), status: :ok }
+      format.html { render html: html_status('green'), status: :ok }
       format.json { render json: {}, status: :ok }
     end
   end
 
   def render_down(exception)
     respond_to do |format|
-      format.html { render html: html_status(color: 'red'), status: :internal_server_error }
+      format.html { render html: html_status('red'), status: :internal_server_error }
       format.json { render json: { error: exception.message }, status: :internal_server_error }
     end
   end
 
   # rubocop:disable Rails/OutputSafety
-  def html_status(color:)
+  def html_status(color)
     %(<html><body style="background-color: #{color}"></body></html>).html_safe
   end
   # rubocop:enable Rails/OutputSafety
