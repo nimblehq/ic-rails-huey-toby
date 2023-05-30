45e4179d3a039cb0ac4a339269385e08cb3a0a41
Tobias (Toby) Heuts <tobias@nimblehq.co>
2023-05-30 15:02:07 +0700

    [#24] More code optimization + update request test

--- spec/requests/health_controller_spec.rb
+++ spec/requests/health_controller_spec.rb
@@ -2,41 +2,44 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Health Controller', type: :request do
+RSpec.describe 'Health Endpoint', type: :request do
   describe 'GET #show' do
     context 'when the application is up' do
       it 'returns a successful response with HTML' do
         get '/'
+
         expect(response).to have_http_status(:ok)
-        expect(response.content_type).to eq('text/html; charset=utf-8')
         expect(response.body).to include('<html><body style="background-color: green"></body></html>')
       end
 
       it 'returns a successful response with JSON' do
         get '/', headers: { 'ACCEPT' => 'application/json' }
+
         expect(response).to have_http_status(:ok)
-        expect(response.content_type).to eq('application/json; charset=utf-8')
         expect(response.body).to eq({ message: 'OK' }.to_json)
       end
     end
 
     context 'when the application is down' do
-      before(:each) do
-        error = StandardError.new('Something went wrong')
-        allow(HealthController).to receive(:show).and_raise(error)
-      end
-
       it 'returns an internal server error response with HTML' do
+        controller_instance = HealthController.new
+        allow(HealthController).to receive(:new).and_return(controller_instance)
+        allow(controller_instance).to receive(:show).and_raise(StandardError, 'Something went wrong')
+
         get '/'
+
         expect(response).to have_http_status(:internal_server_error)
-        expect(response.content_type).to eq('text/html; charset=utf-8')
         expect(response.body).to include('<html><body style="background-color: red"></body></html>')
       end
 
       it 'returns an internal server error response with JSON' do
+        controller_instance = HealthController.new
+        allow(HealthController).to receive(:new).and_return(controller_instance)
+        allow(controller_instance).to receive(:show).and_raise(StandardError, 'Something went wrong')
+
         get '/', headers: { 'ACCEPT' => 'application/json' }
+
         expect(response).to have_http_status(:internal_server_error)
-        expect(response.content_type).to eq('application/json; charset=utf-8')
         expect(response.body).to eq({ error: 'Something went wrong' }.to_json)
       end
     end
