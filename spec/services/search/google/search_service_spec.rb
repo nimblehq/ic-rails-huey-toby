e0040021ed15e359f75a44047a275ed96abbd724
hoangnguyen92dn <hoang.l@nimblehq.co>
2023-06-06 14:06:57 +0700

    [#14] Renaming testcases

--- spec/services/search/google/search_service_spec.rb
+++ spec/services/search/google/search_service_spec.rb
@@ -5,14 +5,14 @@ require 'rails_helper'
 RSpec.describe Search::Google::SearchService, type: :service do
   describe '#call' do
     context 'given the search result is valid' do
-      it 'returns search page body', vcr: 'search_service/keyword_ruby' do
+      it 'returns search page body', vcr: 'services/search/google/valid' do
         search_result = described_class.new('ruby').call
 
         expect(search_result).to include('<title>ruby - Google Search</title>')
       end
     end
 
-    context 'given the search result is invalid' do
+    context 'given an INVALID search result' do
       it 'returns nil' do
         stub_request(:any, /www\.google\.com/).to_return(status: 404, body: '', headers: {})
 
