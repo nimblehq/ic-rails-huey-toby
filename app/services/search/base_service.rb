1f622016499a372272c22de2074e898d5384cfd4
hoangnguyen92dn <hoang.l@nimblehq.co>
2023-06-06 15:28:21 +0700

    [#14] Refactor search service

--- app/services/search/base_service.rb
+++ app/services/search/base_service.rb
@@ -2,11 +2,20 @@
 
 module Search
   class BaseService
-    attr_reader :keyword, :language
-
-    def initialize(keyword, language = 'en')
-      @keyword = keyword
-      @language = language
+    def initialize(search_url)
+      @search_url = search_url
     end
+
+    def call
+      uri = URI.parse(search_url)
+
+      response = Net::HTTP.get_response(uri)
+
+      response.body if response.is_a?(Net::HTTPSuccess)
+    end
+
+    private
+
+    attr_reader :search_url
   end
 end
--- app/services/search/google/search_service.rb
+++ app/services/search/google/search_service.rb
@@ -3,18 +3,9 @@
 module Search
   module Google
     class SearchService < Search::BaseService
-      GOOGLE_SEARCH_BASE_URL = 'https://www.google.com/search'
-
       def initialize(keyword, language = 'en')
-        super(keyword, language)
-      end
-
-      def call
-        uri = URI.parse("#{GOOGLE_SEARCH_BASE_URL}?q=#{keyword}&hl=#{language}&lr=#{language}")
-
-        response = Net::HTTP.get_response(uri)
-
-        response.body if response.is_a?(Net::HTTPSuccess)
+        search_url = "https://www.google.com/search?q=#{keyword}&hl=#{language}&lr=#{language}"
+        super(search_url)
       end
     end
   end
