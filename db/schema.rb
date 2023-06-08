e661dc869ae3ae9ad135cbb73f269ebb49bef08c
hoangnguyen92dn <hoang.l@nimblehq.co>
2023-06-08 12:39:25 +0700

    [#14] Add new html_code column migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_30_101854) do
+ActiveRecord::Schema.define(version: 2023_06_08_053321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 2023_05_30_101854) do
   create_table "search_results", force: :cascade do |t|
     t.string "keyword"
     t.string "search_engine"
-    t.text "html_code"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "html_code"
   end
 
 end
