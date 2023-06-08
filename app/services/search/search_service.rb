6cbab0e93ad7e0d2dd51ee89fc41964f4145d299
hoangnguyen92dn <hoang.l@nimblehq.co>
2023-06-08 17:41:25 +0700

    [#14] Use symbol in case-when rather than string

--- app/services/search/search_service.rb
+++ app/services/search/search_service.rb
@@ -3,8 +3,8 @@
 module Search
   class SearchService
     def self.new(search_engine:, keyword:, language: 'en')
-      case search_engine
-      when 'google'
+      case search_engine.to_sym
+      when :google
         Search::GoogleSearchService.new(keyword, language)
       else
         raise NotImplementedError
