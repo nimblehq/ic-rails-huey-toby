117ce5ac101890c0cd3f650d12114dc5985c80bb
hoangnguyen92dn <hoang.l@nimblehq.co>
2023-06-13 23:00:11 +0700

    [#15] Add rough implementation to parse the search result page

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby] # Windows doe
 gem 'devise' # Authentication solution for Rails with Warden
 gem 'pundit' # Minimal authorization through OO design and pure Ruby classes
 
+gem 'nokogiri' # HTML, XML, SAX, and Reader parser
+
 # Translations
 # gem 'devise-i18n' # Translations for Devise
 # gem 'rails-i18n', '~> 6.0.0' # Translations for Rails
--- app/services/search/google_search_service.rb
+++ app/services/search/google_search_service.rb
@@ -2,7 +2,11 @@
 
 module Search
   class GoogleSearchService
+    attr_reader :raw_html, :adwords_top_count, :adwords_total_count, :adwords_top_urls, :non_adwords_count, :non_adwords_urls,
+                :total_links_count
+
     GOOGLE_SEARCH_URL = 'https://www.google.com/search?q=%{keyword}&hl=%{language}&lr=%{language}'
+    USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
 
     def initialize(keyword, language = 'en')
       @search_url = format(GOOGLE_SEARCH_URL, keyword: keyword, language: language)
@@ -11,9 +15,9 @@ module Search
     def search
       uri = URI.parse(@search_url)
 
-      response = Faraday.get(uri)
+      response = Faraday.get(uri, nil, 'User-Agent' => USER_AGENT)
 
-      response.body if response.status == 200
+      @raw_html = response.body if response.status == 200
     rescue Faraday::ConnectionFailed
       raise IcRailsHueyToby::Errors::SearchServiceError
     end
