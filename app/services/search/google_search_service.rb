7a3d384854c5c1d09d03a4f965f5278273ea2663
hoangnguyen92dn <hoang.l@nimblehq.co>
2023-06-14 23:34:05 +0700

    [#15] Return search result with scraped information from raw html code

--- app/services/search/google_search_service.rb
+++ app/services/search/google_search_service.rb
@@ -2,11 +2,20 @@
 
 module Search
   class GoogleSearchService
-    attr_reader :raw_html, :adwords_top_count, :adwords_total_count, :adwords_top_urls, :non_adwords_count, :non_adwords_urls,
-                :total_links_count
-
     GOOGLE_SEARCH_URL = 'https://www.google.com/search?q=%{keyword}&hl=%{language}&lr=%{language}'
+
+    USER_AGENT_HEADER = 'User-Agent'
     USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
+
+    KEY_DIV_TOP_ADWORDS_CONTAINER = 'div.pla-unit-container'
+    KEY_DIV_NORMAL_ADWORDS_CONTAINER = 'div.v5yQqb'
+    KEY_DIV_NON_ADWORDS_CONTAINER = 'div.yuRUbf'
+
+    KEY_A_TAG = 'a'
+    KEY_A_TAG_ADWORDS_TOP_CLASS = 'a[data-impdclcc]'
+    KEY_A_TAG_HYPERLINK = 'a[href]'
+
+    KEY_HREF_ATTRIBUTE = 'href'
 
     def initialize(keyword, language = 'en')
       @search_url = format(GOOGLE_SEARCH_URL, keyword: keyword, language: language)
@@ -15,48 +24,79 @@ module Search
     def search
       uri = URI.parse(@search_url)
 
-      response = Faraday.get(uri, nil, 'User-Agent' => USER_AGENT)
+      response = Faraday.get(uri, nil, USER_AGENT_HEADER => USER_AGENT)
 
-      @raw_html = response.body if response.status == 200
+      @html_code = response.body if response.status == 200
+
+      parse_html(@html_code)
     rescue Faraday::ConnectionFailed
       raise IcRailsHueyToby::Errors::SearchServiceError
     end
 
     private
 
-    def parse_html
-      doc = Nokogiri::HTML(@raw_html)
-      parse_adwords(doc)
-      parse_non_adwords(doc)
-    end
+    # rubocop:disable Metrics/MethodLength
+    def parse_html(html_code)
+      doc = Nokogiri::HTML(html_code)
 
-    def parse_adwords(doc)
-      parse_top_adwords(doc)
-      parse_normal_adwords(doc)
+      adwords_top_urls = parse_top_adwords(doc)
+      adwords_normal_urls = parse_normal_adwords(doc)
+      non_adwords_urls = parse_non_adwords(doc)
+
+      adwords_top_count = adwords_top_urls.size
+      adwords_normal_count = adwords_normal_urls.size
+      adwords_total_count = adwords_top_count + adwords_normal_count
+      non_adwords_count = non_adwords_urls.size
+      total_links_count = parse_all_links(doc)
+
+      SearchResult.new(
+        adwords_top_urls: adwords_top_urls,
+        adwords_top_count: adwords_top_count,
+        non_adwords_urls: non_adwords_urls,
+        non_adwords_count: non_adwords_count,
+        adwords_total_count: adwords_total_count,
+        total_links_count: total_links_count,
+        html_code: html_code,
+        search_engine: 'google',
+        keyword: @keyword
+      )
     end
+    # rubocop:enable Metrics/MethodLength
 
     def parse_top_adwords(doc)
-      top_ads_container = doc.css('div.pla-unit-container')
+      top_ads_container = doc.css(KEY_DIV_TOP_ADWORDS_CONTAINER)
 
-      @adwords_top_urls = []
+      adwords_top_urls = []
       top_ads_container.each do |adwords_top|
-        @adwords_top_urls << adwords_top.css('a[data-impdclcc]').first['href']
+        adwords_top_urls << adwords_top.css(KEY_A_TAG_ADWORDS_TOP_CLASS).first[KEY_HREF_ATTRIBUTE]
       end
 
-      @adwords_top_count = adwords_top_urls.size
+      adwords_top_urls
     end
 
     def parse_normal_adwords(doc)
-      normal_adwords_count = doc.css('div[data-text-ad]').count
+      normal_ads_container = doc.css(KEY_DIV_NORMAL_ADWORDS_CONTAINER)
 
-      @adwords_total_count = @adwords_top_count + normal_adwords_count
+      adwords_normal_urls = []
+      normal_ads_container.each do |adwords_normal|
+        adwords_normal_urls << adwords_normal.css(KEY_A_TAG).first[KEY_HREF_ATTRIBUTE]
+      end
+
+      adwords_normal_urls
     end
 
     def parse_non_adwords(doc)
-      @non_adwords_urls = []
-      doc.css('#search').css('div.yuRUbf').each do |non_adwords|
-        @non_adwords_urls << non_adwords.css('a').first['href']
+      non_adwords_urls = []
+
+      doc.css(KEY_DIV_NON_ADWORDS_CONTAINER).each do |non_adwords|
+        non_adwords_urls << non_adwords.css(KEY_A_TAG).first[KEY_HREF_ATTRIBUTE]
       end
+
+      non_adwords_urls
+    end
+
+    def parse_all_links(doc)
+      doc.css(KEY_A_TAG_HYPERLINK).size
     end
   end
 end
