692ee1f2d0417c7219f2084be17f363429410ada
hoangnguyen92dn <hoang.l@nimblehq.co>
2023-06-20 12:50:24 +0700

    [#15] Combine all expectations into a single statement

--- spec/services/parse/google_parse_service_spec.rb
+++ spec/services/parse/google_parse_service_spec.rb
@@ -12,31 +12,31 @@ RSpec.describe Parse::GoogleParseService, type: :service do
         parse_service = described_class.new(html_code)
         parse_result = parse_service.parse
 
-        expect(parse_result[:adwords_top_urls]).to eq(
-          [
-            'https://mihanoi.vn/san-pham/smart-tivi-xiaomi-a2-32-inch-chinh-hang',
-            'https://hc.com.vn/ords/p--tivi-samsung-ua65au7000',
-            'https://www.samsung.com/vn/tvs/uhd-4k-tv/au7000-uhd-4k-smart-tv-55-inch-ua55au7000kxxv/',
-            'https://hc.com.vn/ords/p--tivi-lg-43up7550ptc'
-          ]
+        expect(parse_result).to eq(
+          {
+            adwords_top_urls: [
+              'https://mihanoi.vn/san-pham/smart-tivi-xiaomi-a2-32-inch-chinh-hang',
+              'https://hc.com.vn/ords/p--tivi-samsung-ua65au7000',
+              'https://www.samsung.com/vn/tvs/uhd-4k-tv/au7000-uhd-4k-smart-tv-55-inch-ua55au7000kxxv/',
+              'https://hc.com.vn/ords/p--tivi-lg-43up7550ptc'
+            ],
+            adwords_top_count: 4,
+            adwords_total_count: 4,
+            non_adwords_urls: [
+              'https://www.dienmayxanh.com/tivi',
+              'https://www.nguyenkim.com/tivi/',
+              'https://dienmaycholon.vn/tivi',
+              'https://cellphones.com.vn/tivi.html',
+              'https://mediamart.vn/tivi',
+              'https://hc.com.vn/ords/c--tivi',
+              'https://tiki.vn/tivi/c5015',
+              'https://pico.vn/tivi-nhom-157.html',
+              'https://dienmaygiare.net/tivi/'
+            ],
+            non_adwords_count: 9,
+            total_links_count: 13
+          }
         )
-        expect(parse_result[:adwords_top_count]).to eq(4)
-        expect(parse_result[:adwords_total_count]).to eq(4)
-        expect(parse_result[:non_adwords_urls]).to eq(
-          [
-            'https://www.dienmayxanh.com/tivi',
-            'https://www.nguyenkim.com/tivi/',
-            'https://dienmaycholon.vn/tivi',
-            'https://cellphones.com.vn/tivi.html',
-            'https://mediamart.vn/tivi',
-            'https://hc.com.vn/ords/c--tivi',
-            'https://tiki.vn/tivi/c5015',
-            'https://pico.vn/tivi-nhom-157.html',
-            'https://dienmaygiare.net/tivi/'
-          ]
-        )
-        expect(parse_result[:non_adwords_count]).to eq(9)
-        expect(parse_result[:total_links_count]).to eq(13)
       end
     end
 
