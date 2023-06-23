6003337b874c9c839c2e5653c823e57333c22e85
hoangnguyen92dn <hoang.l@nimblehq.co>
2023-06-23 11:11:11 +0700

    [#26] Get rid of x-forwarded-for in request header

--- app/services/search/google_search_service.rb
+++ app/services/search/google_search_service.rb
@@ -6,16 +6,14 @@ module Search
 
     USER_AGENT_HEADER = 'User-Agent'
 
-    X_FORWARDED_FOR_HEADER = 'X-Forwarded-For'
-
     def initialize(keyword, language = 'en')
       @search_url = format(GOOGLE_SEARCH_URL, keyword: keyword, language: language)
     end
 
     def search
-      connection = create_connection(@search_url)
+      uri = URI.parse(@search_url)
 
-      response = connection.get
+      response = Faraday.get(uri, nil, USER_AGENT_HEADER => generate_user_agent)
 
       response.body if response.status == 200
     rescue Faraday::ConnectionFailed
@@ -24,24 +22,8 @@ module Search
 
     private
 
-    def create_connection(search_url)
-      uri = URI.parse(search_url)
-      Faraday.new(url: uri, headers: create_request_header)
-    end
-
-    def create_request_header
-      {
-        USER_AGENT_HEADER => generate_user_agent,
-        X_FORWARDED_FOR_HEADER => generate_ip_address
-      }
-    end
-
     def generate_user_agent
       Ronin::Web::UserAgents.chrome.random
-    end
-
-    def generate_ip_address
-      FFaker::Internet.ip_v4_address
     end
   end
 end
