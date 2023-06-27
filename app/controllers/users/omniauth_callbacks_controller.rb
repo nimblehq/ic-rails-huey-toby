c81f71bd7f8a39b3c69abf12a19762e1f536c455
hoangnguyen92dn <hoang.l@nimblehq.co>
2023-06-27 10:04:13 +0700

    [#9] Add omniauth callbacks controller

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  # You should configure your model like this:
+  # devise :omniauthable, omniauth_providers: [:twitter]
+
+  def google_oauth2
+    @user = User.from_omniauth(auth)
+
+    if user.present?
+      # TODO: Handle success case
+    else
+      # TODO: Handle error
+    end
+  end
+
+  private
+
+  def auth
+    @auth ||= request.env['omniauth.auth']
+  end
+
+  # You should also create an action method in this controller like this:
+  # def twitter
+  # end
+
+  # More info at:
+  # https://github.com/heartcombo/devise#omniauth
+
+  # GET|POST /resource/auth/twitter
+  # def passthru
+  #   super
+  # end
+
+  # GET|POST /users/auth/twitter/callback
+  # def failure
+  #   super
+  # end
+
+  # protected
+
+  # The path used when OmniAuth fails
+  # def after_omniauth_failure_path_for(scope)
+  #   super(scope)
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks'
+   }
   mount Sidekiq::Web => "/sidekiq" if Rails.env.development?
 
   namespace :api do
