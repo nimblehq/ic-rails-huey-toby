181658deaad3f79c5efa03542523e461f023ba87
hoangnguyen92dn <hoang.l@nimblehq.co>
2023-06-28 18:11:22 +0700

    [#9] Return responses on oauth2 callback

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,46 +1,27 @@
 # frozen_string_literal: true
 
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
+module Users
+  class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    respond_to :json
+    def google_oauth2
+      @user = User.from_omniauth(auth)
 
-  def google_oauth2
-    @user = User.from_omniauth(auth)
+      if @user.persisted?
+        render_success
+      else
+        render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
 
-    if user.present?
-      # TODO: Handle success case
-    else
-      # TODO: Handle error
+    private
+
+    def auth
+      @auth ||= request.env['omniauth.auth']
+    end
+
+    def render_success
+      # TODO: Return JWT token
+      render json: { success: true }
     end
   end
-
-  private
-
-  def auth
-    @auth ||= request.env['omniauth.auth']
-  end
-
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
-
-  # More info at:
-  # https://github.com/heartcombo/devise#omniauth
-
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
-
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
-
-  # protected
-
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
 end
