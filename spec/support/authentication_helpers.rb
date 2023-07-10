b40a28c3a0508be306bd1e9f87d231819c546847
hoangnguyen92dn <hoang.l@nimblehq.co>
2023-07-10 20:26:54 +0700

    [#13] Update UnitTest

--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -2,8 +2,7 @@
 
 module Request
   module AuthenticationHelpers
-    def post_authenticated_request(endpoint:, params: {})
-      user = Fabricate(:user)
+    def post_authenticated_request(user:, endpoint:, params: {})
       application = Fabricate(:application)
       access_token = Fabricate(:access_token, resource_owner_id: user.id, application_id: application.id)
 
@@ -12,6 +11,17 @@ module Request
       }
 
       post endpoint, headers: headers, params: params
+    end
+
+    def get_authenticated_request(user:, endpoint:, params: {})
+      application = Fabricate(:application)
+      access_token = Fabricate(:access_token, resource_owner_id: user.id, application_id: application.id)
+
+      headers = {
+        Authorization: "Bearer #{access_token.token}"
+      }
+
+      get endpoint, headers: headers, params: params
     end
   end
 end
