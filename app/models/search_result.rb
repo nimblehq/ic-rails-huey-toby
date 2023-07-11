92f07b5a21aa10c0aad9fd04b7dca3c2b3bd245b
hoangnguyen92dn <hoang.l@nimblehq.co>
2023-07-11 08:53:47 +0700

    [#13] Code clean up

--- app/models/search_result.rb
+++ app/models/search_result.rb
@@ -4,9 +4,9 @@ class SearchResult < ApplicationRecord
   enum search_engine: { google: 'google', bing: 'bing' }
   enum status: { in_progress: 'in_progress', completed: 'completed', failed: 'failed' }
 
+  belongs_to :user
+
   validates :keyword, :search_engine, :status, presence: true
   validates :search_engine, inclusion: { in: search_engines.keys }
   validates :status, inclusion: { in: statuses.keys }
-
-  belongs_to :user
 end
