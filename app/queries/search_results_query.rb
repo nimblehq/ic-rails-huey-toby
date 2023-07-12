d99e9f08e543a9dbdd55ad6aa078bc833c2e6b41
Tobias (Toby) Heuts <tobias@nimblehq.co>
2023-07-12 12:32:56 +0700

    [#17] Add filter to check if Adwords urls contains a word

--- app/queries/search_results_query.rb
+++ app/queries/search_results_query.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 CONDITION_URL_EQUALS = ':url = ANY (adwords_top_urls) OR :url = ANY (non_adwords_urls)'
+CONDITION_ADWORDS_URL_CONTAINS = "array_to_string(adwords_top_urls, ', ') LIKE :word"
 
 class SearchResultsQuery
-  attr_reader :search_results, :filters, :sorts
+  attr_reader :search_results, :filters
 
   def initialize(search_results = nil, filters = {})
     @search_results = search_results || SearchResult.all
@@ -12,6 +13,7 @@ class SearchResultsQuery
 
   def call
     @search_results = filter_by_url(filters[:url_equals])
+    @search_results = filter_adwords_url_contains(filters[:adwords_url_contains])
 
     @search_results
   end
@@ -19,6 +21,14 @@ class SearchResultsQuery
   def filter_by_url(url)
     if url.present?
       @search_results.where(CONDITION_URL_EQUALS, url: url)
+    else
+      @search_results
+    end
+  end
+
+  def filter_adwords_url_contains(word)
+    if word.present?
+      @search_results.where(CONDITION_ADWORDS_URL_CONTAINS, word: "%#{word}%")
     else
       @search_results
     end
--- spec/queries/search_results_query_spec.rb
+++ spec/queries/search_results_query_spec.rb
@@ -7,10 +7,10 @@ RSpec.describe SearchResultsQuery, type: :model do
     context 'given NO filters' do
       it 'returns all search results' do
         user = Fabricate(:user)
-        Fabricate(:search_result, user_id: user.id, non_adwords_urls: ['wwww.nimblehq.co'])
-        Fabricate(:search_result, user_id: user.id, adwords_top_urls: ['wwww.nimblehq.co'])
-        Fabricate(:search_result, user_id: user.id, non_adwords_urls: ['wwww.nimblehq.co/compass'])
-        Fabricate(:search_result, user_id: user.id, adwords_top_urls: ['wwww.nimblehq.co/compass'])
+        Fabricate(:search_result, user_id: user.id, non_adwords_urls: ['www.nimblehq.co'])
+        Fabricate(:search_result, user_id: user.id, adwords_top_urls: ['www.nimblehq.co'])
+        Fabricate(:search_result, user_id: user.id, non_adwords_urls: ['www.nimblehq.co/compass'])
+        Fabricate(:search_result, user_id: user.id, adwords_top_urls: ['www.nimblehq.co/compass'])
         search_results_query = described_class.new
 
         search_results_query.call
@@ -22,15 +22,30 @@ RSpec.describe SearchResultsQuery, type: :model do
     context 'given url_equals filter' do
       it 'returns only the search results that are equal to the url' do
         user = Fabricate(:user)
-        Fabricate(:search_result, user_id: user.id, non_adwords_urls: ['wwww.nimblehq.co'])
-        Fabricate(:search_result, user_id: user.id, adwords_top_urls: ['wwww.nimblehq.co'])
-        Fabricate(:search_result, user_id: user.id, non_adwords_urls: ['wwww.nimblehq.co/compass'])
-        Fabricate(:search_result, user_id: user.id, adwords_top_urls: ['wwww.nimblehq.co/compass'])
-        search_results_query = described_class.new(nil, { url_equals: 'wwww.nimblehq.co' })
+        Fabricate(:search_result, user_id: user.id, non_adwords_urls: ['www.nimblehq.co'])
+        Fabricate(:search_result, user_id: user.id, adwords_top_urls: ['www.nimblehq.co'])
+        Fabricate(:search_result, user_id: user.id, non_adwords_urls: ['www.nimblehq.co/compass'])
+        Fabricate(:search_result, user_id: user.id, adwords_top_urls: ['www.nimblehq.co/compass'])
+        search_results_query = described_class.new(nil, { url_equals: 'www.nimblehq.co' })
 
         search_results_query.call
 
         expect(search_results_query.search_results.count).to eq 2
+      end
+    end
+
+    context 'given adwords_url_contains filter' do
+      it 'returns only the search results which contain the word' do
+        user = Fabricate(:user)
+        Fabricate(:search_result, user_id: user.id, non_adwords_urls: ['www.nimblehq.co'])
+        Fabricate(:search_result, user_id: user.id, adwords_top_urls: ['www.nimblehq.co'])
+        Fabricate(:search_result, user_id: user.id, non_adwords_urls: ['www.nimblehq.co/compass'])
+        Fabricate(:search_result, user_id: user.id, adwords_top_urls: ['www.nimblehq.co/compass'])
+        search_results_query = described_class.new(nil, { adwords_url_contains: 'compass' })
+
+        search_results_query.call
+
+        expect(search_results_query.search_results.count).to eq 1
       end
     end
   end
